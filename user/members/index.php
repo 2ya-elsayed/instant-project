fc04b60bcda799e39ef688324961fdfb86d0ae44
ziadA7med12 <ziadshafik2001@gmail.com>
2023-07-27 13:12:57 +0300

    First Commit

---
+++ user/members/index.php
@@ -0,0 +1,57 @@
+<?php
+include "../shared/head.php";
+include "../shared/header.php";
+include "../shared/Sidebar.php";
+include "../general/connection.php";
+include "../general/function.php";
+?>
+<div class="home-profile">
+    <div class="title-home">
+        <i class="fa-solid fa-user"></i>
+        <h2>Members</h2>
+    </div>
+
+    <div class="members">
+        <div class="instructor">
+            <div class="title-instructor">
+                <h2>Instructor</h2>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="data">
+                <img src="/instant/user/image/aya.jpeg" alt="">
+                <h2> Aya Elsayed</h2>
+            </div>
+        </div>
+        <div class="student">
+            <div class="title-student">
+                <h2>Student</h2>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="data">
+                <img src="/instant/user/image/DSC_6185.jpg" alt="">
+                <h2> Samaa Abdl-Mohsen </h2>
+            </div>
+            <div class="data">
+                <img src="/instant/user/image/DSC_6188.jpg" alt="">
+                <h2> Alaa Youssef </h2>
+            </div>
+            <div class="data">
+                <img src="/instant/user/image/DSC_6189.jpg" alt="">
+                <h2> Amany Mohamed </h2>
+            </div>
+            <div class="data">
+                <img src="/instant/user/image/p2.jpg" alt="">
+                <h2> Ahmed S.Elfran </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<?php
+include "../shared/script.php"
+?>
